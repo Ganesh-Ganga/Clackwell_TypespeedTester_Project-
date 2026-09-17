0e1ec5ba67ee6f8bbaef815f6c51dc26aaf9e52c
Ganesh Ganga <ganeshgonga04@gmail.com>
2026-09-17 17:10:11 +0530

    Configure PostgreSQL for Render

--- Clackwell/config.py
+++ Clackwell/config.py
@@ -14,20 +14,19 @@ class Config:
 
     DATABASE_URL = os.environ.get("DATABASE_URL")
 
-    if DATABASE_URL:
-        if DATABASE_URL.startswith("postgres://"):
-            DATABASE_URL = DATABASE_URL.replace(
-                "postgres://",
-                "postgresql://",
-                1
-            )
-
-        SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    else:
-        SQLALCHEMY_DATABASE_URI = (
-            "sqlite:///" +
-            os.path.join(BASE_DIR, "database", "database.db")
+    if not DATABASE_URL:
+        raise RuntimeError(
+            "DATABASE_URL environment variable is not set."
         )
+
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
