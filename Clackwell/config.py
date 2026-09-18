8bc74dc4a8bf4c0b9f5430a593f1636dd5c3779b
Ganesh Ganga <ganeshgonga04@gmail.com>
2026-09-18 09:52:39 +0530

    Use SQLite only

--- Clackwell/config.py
+++ Clackwell/config.py
@@ -12,21 +12,11 @@ class Config:
         "clackwell-dev-key-change-me"
     )
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-
-    if not DATABASE_URL:
-        raise RuntimeError(
-            "DATABASE_URL environment variable is not set."
-        )
-
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgres://",
-            "postgresql://",
-            1
-        )
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    # SQLite only
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" +
+        os.path.join(BASE_DIR, "database", "database.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
